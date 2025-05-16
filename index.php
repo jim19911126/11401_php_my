e7cbbeafc01fa045433f945b2b1020c59af8da11
jimlee <jim19911126@gmail.com>
2025-05-16 09:37:48 +0800

    新資料夾

---
+++ index.php
@@ -0,0 +1,9 @@
+<form action="?" method='get'>
+    <input type="text" name="search">
+    <input type="text" value="search">
+</form>
+
+<form action="?" method='post'>
+    <input type="text" name="search">
+    <input type="text" value="search">
+</form>
